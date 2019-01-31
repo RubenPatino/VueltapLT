cb95cdbdb392c98d2ae2f981e5ac6a855f312828
Rapafull <rapafull@gmail.com>
2019-01-31 00:39:13 -0600

    Summary

--- app/src/main/java/com/vueltap/Transport/View/ViewTransport.java
+++ app/src/main/java/com/vueltap/Transport/View/ViewTransport.java
@@ -1,5 +1,12 @@
 package com.vueltap.Transport.View;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,10 +16,15 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.vueltap.Transport.Adapter.AdapterTransport;
 import com.vueltap.R;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import cn.pedant.SweetAlert.SweetAlertDialog;
 
@@ -21,10 +33,13 @@ public class ViewTransport extends AppCompatActivity {
     private RecyclerView recyclerView;
     private AdapterTransport adapter;
     private LinearLayout linearLayout;
-    private CheckBox cbClicla,cbMoto;
+    private CheckBox cbClicla, cbMoto;
     private SweetAlertDialog dialog;
-    private String urlProperty="",urlSOAT="",urlLicence="",numPlaca="";
+    private String urlProperty = "", urlSOAT = "", urlLicence = "", numPlaca = "";
     private EditText etPlaca;
+    private final int PICTURE_RESULT=1;
+    private Uri imageUri;
+    private ImageView ivLicence,ivProperty,ivSoat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +50,10 @@ public class ViewTransport extends AppCompatActivity {
     }
 
     private void loadControls() {
-        etPlaca=findViewById(R.id.TextInputPlaca);
+        etPlaca = findViewById(R.id.TextInputPlaca);
+        ivLicence=findViewById(R.id.imageViewLicence);
+        ivProperty=findViewById(R.id.imageViewProperty);
+        ivSoat=findViewById(R.id.imageViewSoat);
        /* recyclerView=findViewById(R.id.rvTypeTransport);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -56,101 +74,138 @@ public class ViewTransport extends AppCompatActivity {
             }
         });*/
 
-       linearLayout=findViewById(R.id.LinearLayoutTransport);
-       cbClicla=findViewById(R.id.checkBoxBicicleta);
-       cbMoto=findViewById(R.id.checkBoxMoto);
+        linearLayout = findViewById(R.id.LinearLayoutTransport);
+        cbClicla = findViewById(R.id.checkBoxBicicleta);
+        cbMoto = findViewById(R.id.checkBoxMoto);
 
-       cbClicla.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-           @Override
-           public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-               validateCheck();
-           }
-       });
-       cbMoto.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-           @Override
-           public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-               validateCheck();
-           }
-       });
+        cbClicla.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                validateCheck();
+            }
+        });
+        cbMoto.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                validateCheck();
+            }
+        });
 
     }
 
-    private Boolean validateMoto(View view){
-        numPlaca=etPlaca.getText().toString().trim();
-        if(numPlaca.isEmpty()){
+    private Boolean validateMoto(View view) {
+        numPlaca = etPlaca.getText().toString().trim();
+        if (numPlaca.isEmpty()) {
             etPlaca.setError("Completa este campo");
             etPlaca.requestFocus();
             return false;
-        }else if(urlLicence.isEmpty()){
+        } else if (urlLicence.isEmpty()) {
             OnClickHelpLicence(view);
             return false;
-        }else if(urlLicence.isEmpty()){
+        } else if (urlProperty.isEmpty()) {
             OnClickHelpProperty(view);
             return false;
-        }else if(urlSOAT.isEmpty()){
+        } else if (urlSOAT.isEmpty()) {
             OnClickHelpSoat(view);
             return false;
-        }else{
+        } else {
             return true;
         }
     }
 
     private void validateCheck() {
-        if(cbMoto.isChecked()){
+        if (cbMoto.isChecked()) {
             linearLayout.setVisibility(View.VISIBLE);
-        }else{
+        } else {
             linearLayout.setVisibility(View.GONE);
         }
 
     }
-    public void OnclickPhotoSoat(View view){
-        urlSOAT="url";
-    }
-    public void OnclickPhotoLicence(View view){
-        urlLicence="url";
-    }
-    public void OnclickPhotoProperty(View view){
-        urlProperty="url";
+
+    private void takePic() {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File file = new File(Environment.getExternalStorageDirectory(), ".temp" + timeStamp + ".png");
+        imageUri = Uri.fromFile(file);
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, PICTURE_RESULT);
     }
 
-    public void OnClickHelpSoat(View view){
-        dialog=new SweetAlertDialog(this,SweetAlertDialog.WARNING_TYPE);
+    public void OnclickPhotoSoat(View view) {
+        urlSOAT = "url";
+    }
+
+    public void OnclickPhotoLicence(View view) {
+        takePic();
+        //urlLicence = "url";
+    }
+
+    public void OnclickPhotoProperty(View view) {
+        urlProperty = "url";
+    }
+
+    public void OnClickHelpSoat(View view) {
+        dialog = new SweetAlertDialog(this, SweetAlertDialog.WARNING_TYPE);
         dialog.setContentText("Tomale una foto a tu SOAP");
         dialog.show();
     }
-    public void OnClickHelpLicence(View view){
-        dialog=new SweetAlertDialog(this,SweetAlertDialog.WARNING_TYPE);
+
+    public void OnClickHelpLicence(View view) {
+        dialog = new SweetAlertDialog(this, SweetAlertDialog.WARNING_TYPE);
         dialog.setContentText("Tomale una foto a tu licencia de conducir");
         dialog.show();
 
     }
-    public void OnClickHelpProperty(View view){
-        dialog=new SweetAlertDialog(this,SweetAlertDialog.WARNING_TYPE);
+
+    public void OnClickHelpProperty(View view) {
+        dialog = new SweetAlertDialog(this, SweetAlertDialog.WARNING_TYPE);
         dialog.setContentText("Tomale una foto a tu tarjeta de propidad");
         dialog.show();
     }
-    public void OnClickRegister(View view){
-        if(cbMoto.isChecked()){
-            if(validateMoto(view)){
-                Log.d("TAG","moto, todo OK");
+
+    public void OnClickRegister(View view) {
+        if (cbMoto.isChecked()) {
+            if (validateMoto(view)) {
+                Log.d("TAG", "moto, todo OK");
             }
-        }else if(cbClicla.isChecked()){
-            Log.d("TAG","cicla, todo OK");
-        }else{
-            Log.d("TAG","Por favor selecciona alguna.");
         }
-        if(cbClicla.isChecked() && cbMoto.isChecked()){
-            if(validateMoto(view)){
-
-            }
-        }else if(cbClicla.isChecked()){
-
-        }else if(cbMoto.isChecked()){
-            if(validateMoto(view)){
-
-            }
-        }else{
-
+        if (cbClicla.isChecked()) {
+            Log.d("TAG", "cicla, todo OK");
+        }
+        if(!cbMoto.isChecked() && !cbClicla.isChecked()){
+            Log.d("TAG", "Por favor selecciona alguna.");
         }
     }
+
+    public String getRealPathFromURI(Uri contentUri) {
+        String[] proj = {MediaStore.Images.Media.DATA};
+        Cursor cursor = managedQuery(contentUri, proj, null, null, null);
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
+    }
+
+
+
+
+    /*public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+
+            case PICTURE_RESULT:
+                if (requestCode == PICTURE_RESULT)
+                    if (resultCode == Activity.RESULT_OK) {
+                        try {
+                            Bitmap thumbnail = MediaStore.Images.Media.getBitmap(
+                                    getContentResolver(), imageUri);
+                            ivLicence.setImageBitmap(thumbnail);
+                            String imageurl = getRealPathFromURI(imageUri);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+        }
+    }*/
 }
+
