d1923ada8bb9827214364783d9887d8cda83a971
Rapafull <rapafull@gmail.com>
2019-02-01 16:23:34 -0600

    update

--- app/src/main/java/com/vueltap/System/Constant.java
+++ app/src/main/java/com/vueltap/System/Constant.java
@@ -32,55 +32,7 @@ public class Constant {
     public static final String DRIVER_LICENSE="driver_license";
     public static final String SOAT="soat";
     public static final String TECNOMECANICA="tecnomecanica";
-
     public static final String INFORMATION="information";
     public static final String URL_INFORMATION="urlinformation";
-
-
-
-
-
-
-    public static final String _IP = "DATA_IP";
-    public static final String SYSTEM_IP = "SYSTEM_IP";
-    public static final String _CONFIGURACION = "DATA_CONFIG";
-    public static final String ROUTE_ID_ADMIN = "ROUTE_ID_ADMIN";
-    public static final String ROUTE_ID = "ROUTE_ID";
-    public static final String ROUTE_NAME = "ROUTE_NAME";
-    public static final String ROUTE_CITY = "ROUTE_CITY";
-    public static final String ROUTE_USER ="ROUTE_USER" ;
-    public static final String ROUTE_PASS = "ROUTE_PASS";
-    public static final String MESSENGER_ID = "MESSENGER_ID";
-    public static final String CLIENT_NAME = "CLIENT_NAME";
-    public static final String CLIENT_LAST_NAME = "CLIENT_LAST_NAME";
-    public static final String CLIENT_ADDRESS = "CLIENT_ADDRESS";
-    public static final String CLIENT_PHONE = "CLIENT_PHONE";
-    public static final String CREDIT_ID = "CREDIT_ID";
-    public static final String CREDIT_VALUE = "CREDIT_VALUE";
-    public static final String CREDIT_DATE = "CREDIT_DATE";
-    public static final String CREDIT_VALUE_FEE = "CREDIT_VALUE_FEE";
-    public static final String CREDIT_ULTIMATE_PAY = "CREDIT_ULTIMATE_PAY";
-    public static final String CREDIT_BALANCE = "CREDIT_BALANCE";
-    public static final String CREDIT_PAID_FEES = "CREDIT_PAID_FEES";
-    public static final String CREDIT_VALUE_NET = "CREDIT_AMOUNT_NET";
-    public static final String CREDIT_TIME= "CREDIT_TIME";
-    public static final String CREDIT_PERCENTAGE = "CREDIT_PERCENTAGE";
-    public static final String CREDIT_INITIAL_PAYMENT = "CREDIT_INITIAL_PAYMENT";
-    public static final String PAYMENT_ID = "PAYMENT_ID";
-    public static final String VALUE_ZERO = "0";
-    public static final String VALUE_ONE = "1";
-    public static final int REQUEST_CODE=0;
-    public static final String SPEND_ID="SPEND_ID";
-    public static final String SPEND_CONCEPT="SPEND_CONCEPT";
-    public static final String SPEND_AMOUNT="SPEND_AMOUNT";
-    public static final int DEFAULT_INDEX = 0;
-    public static final int RESULT_DELETE=2;
-    public static final int RESULT_CANCEL=0;
-    public static final int ZERO_VALUE=0;
-    //AMAZON
-    public static final String DEFAULT_IP_VALUE = "18.221.120.85";
-    //LOCAL HOST
-    public static final String API_BASE_URL = "http://192.168.100.2/:3000";
-    //AWARDSPACE
-    //public static final String API_BASE_URL ="http://cobramex.myartsonline.com/public/";
-}
+ public static final String VALUE_ZERO = "0";
+ }
