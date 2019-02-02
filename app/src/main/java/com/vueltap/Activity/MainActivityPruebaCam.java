10528cbf1087bbb77a998391b137d3e4add6dfab
Rapafull <rapafull@gmail.com>
2019-02-02 01:14:11 -0600

    img_update

--- app/src/main/java/com/vueltap/Activity/MainActivityPruebaCam.java
+++ app/src/main/java/com/vueltap/Activity/MainActivityPruebaCam.java
@@ -2,18 +2,32 @@ package com.vueltap.Activity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.vueltap.BuildConfig;
 import com.vueltap.R;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivityPruebaCam extends AppCompatActivity {
-    private ImageView ivPrueba;
-    private final int REQUEST_CODE =1;
+    private ImageView mImageView;
+    private final int REQUEST_CODE = 1;
+    private String mCurrentPhotoPath;
+    private static final int REQUEST_TAKE_PHOTO = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,25 +37,123 @@ public class MainActivityPruebaCam extends AppCompatActivity {
     }
 
     private void loadControls() {
-        ivPrueba=findViewById(R.id.imageViewPrueba);
+        mImageView = findViewById(R.id.imageViewPrueba);
     }
 
-    public void OnClick(View view){
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent, REQUEST_CODE);
+    public void OnClick(View view) {
+        dispatchTakePictureIntent();
+    }
+
+    public void setFile(Bitmap bmp, String prefix) {
+        //File tempFile = null;
+        //Uri uri = null;
+        try {
+            File tempDir = Environment.getExternalStorageDirectory();
+            tempDir = new File(tempDir.getAbsolutePath() + "/.temp/");
+            tempDir.mkdir();
+            File tempFile = File.createTempFile(prefix + "_", ".jpg", tempDir);
+            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+            bmp.compress(Bitmap.CompressFormat.JPEG, 20, bytes);
+            byte[] bitmapData = bytes.toByteArray();
+            FileOutputStream fos = new FileOutputStream(tempFile);
+            // uri = Uri.fromFile(tempFile);
+            fos.write(bitmapData);
+            fos.flush();
+            fos.close();
+            Log.d("RUTA : ", tempDir.getAbsolutePath());
+            //return tempFile;
+        } catch (IOException e) {
+            e.printStackTrace();
+            // return null;
+        }
+    }
+
+    private void setPic() {
+        // Get the dimensions of the View
+        int targetW = mImageView.getWidth();
+        int targetH = mImageView.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        mImageView.setImageBitmap(bitmap);
+        setFile(bitmap, "IMG");
+
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+        Log.d("Guardada en : ", contentUri.getPath());
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        Log.d("DIR", mCurrentPhotoPath);
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File  ...
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        BuildConfig.APPLICATION_ID + ".fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Log.d("RequestCode = ","_"+requestCode);
-        Log.d("ResultCode = ","_"+requestCode);
-        Log.d("Data = ","_"+data);
-        /*switch (requestCode){
-            case REQUEST_CODE:
-                Bitmap bitmap=data.getExtras().get("data");
+
+        switch (requestCode) {
+            case REQUEST_TAKE_PHOTO:
+                Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath);
+                mImageView.setImageBitmap(bitmap);
+                setFile(bitmap, "IMG");
                 break;
-        }*/
+        }
     }
 }
 
